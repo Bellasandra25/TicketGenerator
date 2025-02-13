2b5e1937c4e2f88135a9ca7336145b607b3d6ec7
Sandra Odubela <odubelaoluwadamilola@gmail.com>
2025-02-13 14:56:38 +0000

    reduced the padding in the header and added the image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center mt-2 p-4 bg-[#033c46] border border-[#1a331a] text-white w-[95%] rounded-lg mb-4'>
-        <img src="" alt="" />
+    <header className='flex justify-between items-center mt-2 p-3 bg-[#033c46] border border-[#1a331a] text-white w-[95%] rounded-lg mb-4'>
+        <div className='flex items-center space-x-2'>
+        <img src="/assets/hugeicons_ticket-01.png" alt="" />
+        <img src="/assets/ticz.png" alt="" />
+        </div>
     
             <ul className='flex justify-between items-center space-x-4'>
                 <li ><a href="http://" target="_blank" rel="noopener noreferrer" className='hover:underline'>Events</a></li>
